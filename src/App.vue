ed0f70928e0b7bab358dd039034bcc1930453044
MattiaBologna <mattiabologna01@gmail.com>
2024-04-12 17:58:38 +0200

    Add: app.vue axios get

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,26 @@
 import HomeHeader from './components/HomeHeader.vue'
 import HomeMain from './components/HomeMain.vue'
 import HomeFooter from './components/HomeFooter.vue'
+import axios from 'axios';
+import { store } from './store.js';
 
 export default {
   components: {
     HomeHeader,
     HomeMain,
     HomeFooter
+  },
+  methods: {
+    fetchData() {
+      axios.get('http://localhost:3000/products').then((res) => {
+        console.log(res.data)
+        console.log(store)
+        store.products = res.data
+      })
+    }
+  },
+  created() {
+    this.fetchData()
   }
 }
 </script>
